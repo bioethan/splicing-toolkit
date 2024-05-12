b77402591289a2c52f22d07650bfefb2971be4d6
Ethan Brown <ethan.allan.brown@gmail.com>
2024-05-12 00:29:56 -0500

    starting to refactor package to make it extensible

--- splicing_toolkit/main.py
+++ splicing_toolkit/main.py
@@ -1,22 +1,3 @@
-from utils import *
+from utils import splicing_classifier
 
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC1_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC1_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC2_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC2_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC3_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC3_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC4_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC4_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC5_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC5_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC6_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC6_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC7_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC7_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC8_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC8_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC9_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC9_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC10_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC10_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC11_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC11_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC12_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC12_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC13_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC13_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC14_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC14_output_splicing_classified')
-splicing_classifier('/Users/ethanbrown/Documents/lab_work/quality_sorted/filtered_BC15_sorted_qc.bed', '/Users/ethanbrown/Documents/splicing_toolkit/output_splicing_classified/BC15_output_splicing_classified')
-
-
-
-
-
+splicing_classifier('LRS_url', 'output_dir')
