e442b2cb9c52c9267219d708a944a66af0d5b5ff
Ethan Brown <ethan.allan.brown@gmail.com>
2024-07-19 02:06:02 -0400

    updated the classifier to use multiprocessing and generally improved documentation and readability

--- tests/test_process_inputs.py
+++ tests/test_process_inputs.py
@@ -1,6 +1,9 @@
 from splicing_classifier.process_inputs import process_gff, \
     process_gff_utrs, parse_long_read_introns_exons
 
+# TODO: Utilize pytest to generate test cases for the functions in
+# process_inputs.py
+
 
 def test_process_gff():
     test_url = 'test_documents/S_pombe_all_chr.gff3'
